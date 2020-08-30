a47a20e078c3b6974e2afcbcb870151c8d671bf4
kurokawa <kurochannofx@gmail.com>
2020-08-30 14:51:40 +0900

    Alamofire追加

--- SampleProject/StartUpViewController.swift
+++ SampleProject/StartUpViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Alamofire
 
 class StartUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginButton: UIButton!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loginButton.rx.tap
+        .subscribe { [unowned self] _ in
+            self.login()
+        }
+        .disposed(by: disposeBag)
+        
     }
+    
+    private func login() {
+        let baseUrl = "http://zipcloud.ibsnet.co.jp/api/"
+        let searchUrl = "\(baseUrl)search"
+        let parameters: [String: Any] = ["zipcode": "2790031"]
+        let headers: HTTPHeaders = ["Content-Type": "application/json"]
+        
+        AF.request(searchUrl, method: .get, parameters: parameters, encoding: URLEncoding(destination: .queryString), headers: headers).responseJSON { response in
+            guard let data = response.data else {
+                return
+            }
+            print(response)
+//            do {
+//                self.addresses = try JSONDecoder().decode(AddressModel.self, from: data)
+//            } catch let error {
+//                print("Error: \(error)")
+//            }
+        }
+    }
+    
     
 
     /*
