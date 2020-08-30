d5ae8e9588dd79058f73cd1da1ea6ee34cae8ba3
kurokawa <kurochannofx@gmail.com>
2020-08-30 14:07:12 +0900

    initial commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by kurokawahirokazu on 2020/08/30.
+//  Copyright © 2020 kurokawahirokazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
